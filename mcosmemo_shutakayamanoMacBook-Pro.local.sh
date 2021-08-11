6e84c4e9557778336bb5449bf5d04f99f1725c5e
Shu Takayama <syu.takayama@gmail.com>
2021-08-11 20:08:41 +0900

    initial commit

---
+++ mcosmemo_shutakayamanoMacBook-Pro.local.sh
@@ -0,0 +1,71 @@
+chsh -s /bin/bash
+
+mkdir ~/.profile.d
+# edit ~/.bash_profile
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# edit ~/.profile.d
+brew tap homebrew/cask-fonts
+
+# install MacPorts
+# edit ~/.profile.d
+
+brew install node
+
+brew install neovim
+npm install -g neovim
+sudo port install py311-neovim
+sudo port select python python311
+# edit ~/.vimrc
+# edit ~/.config/nvim
+# edit ~/.profile.d
+
+# edit ~/.ssh/config
+# edit ~/.profile.d
+
+brew install gnupg
+# edit ~/.profile.d
+
+brew install asdf
+# edit ~/.profile.d
+asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
+asdf plugin-add python
+asdf global golang system
+asdf global python system
+
+# install Google Cloud SDK
+# edit ~/.profile.d
+gcloud components install app-engine-go
+
+# install Android Studio
+# edit ~/.profile.d
+
+brew install font-cica
+
+# ------------------------------------------------------------------------------
+
+mkdir ~/.bashrc.d
+# edit ~/.bashrc
+
+# edit ~/.gitconfig
+
+# edit ~/.bashrc
+source ~/.bashrc
+
+brew install git-lfs
+
+# install JDK
+
+# install Docker Desktop
+
+brew install go
+brew install gopls
+
+npm install -g hexo-cli
+
+# install GIMP
+brew install ghostscript
+
+# install Audacity
+
+brew install font-chewy
